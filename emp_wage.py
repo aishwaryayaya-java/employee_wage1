56e46b799aa4a8234cd45ee3712b9b91f7b79c3e
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-13 16:33:40 +0530

    ADDED Monthly_wages function

--- emp_wage.py
+++ emp_wage.py
@@ -1,39 +1,48 @@
 import random
 print("Welcome to employee wage computation program")
-def check_attendance(attendance):
-    match (attendance):
-        case 0: 
-            print("Employee is absent")
-            dailywage=0  
+import random
+def monthly_wage():
+    total_wage=0
+    for i in range(20):
+        attendance=random.randint(0,1)
+        if attendance==0:
+            continue
+        if attendance==1:
+            job=input('enter a type of job :').lower()
+            if job=='part':
+                total_wage=total_wage+(20*6)
+            elif job=='full':
+                total_wage=total_wage+(20*8)
+            else:
+                print("give job type")
+    return total_wage
+
+print("Total monthly wage:" ,monthly_wage())
+
+
+
+
+
+
+
+# def check_attendance(attendance):
+#     match (attendance):
+#         case 0: 
+#             print("Employee is absent")
+#             dailywage=0  
             
-        case 1: 
-            print("Employee is present")  
-            job=input("Enter full/part time :")
-            match (job):
-                case "full":
-                    dailywage=20*8
+#         case 1: 
+#             print("Employee is present")  
+#             job=input("Enter full/part time :")
+#             match (job):
+#                 case "full":
+#                     dailywage=20*8
                     
-                case "part":
-                    dailywage=20*4
-    return dailywage                      
-num=random.randint(0,1)
-print(check_attendance(num))                   
+#                 case "part":
+#                     dailywage=20*4
+#     return dailywage                      
+# num=random.randint(0,1)
+# print(check_attendance(num))                   
 
 
-"""
-def check_attendance(attendance):
-    if attendance==0:
-        print('employee is absent')
-        dailywage=0
-        print(dailywage)
-    elif attendance==1:
-        print('employee is present')
-        job=input("Enter part or full time :").lower()
-        if(job=="part"):
-            dailywage=20*4
-        if(job=="full"):
-            dailywage=20*8
-        print(dailywage)
-num=random.randint(0,1)
-check_attendance(num)
-"""
+
