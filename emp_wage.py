a16600bbbc3f09d63f6e8611b11a8460c3c54f37
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-14 01:12:20 +0530

    ADDED as Class

--- emp_wage.py
+++ emp_wage.py
@@ -1,45 +1,48 @@
-import random  
-print("Welcome to employee wage computation program")
-wage_hr=20
-full_hr=8
-part_hr=4
-max_hr=100
-max_days=20
-def monthly_wage():
-    total_wage=0
-    total_hrs=0
-    total_days=0
-    while total_hrs<max_hr or total_days<max_days:
-        attendance=random.randint(0,1)
-        if attendance==0:
-          continue
+import random
+
+class EmployeeWage:
+    
+    wage_hr = 20
+    full_hr = 8
+    part_hr = 4
+    max_hr = 100
+    max_days = 20
+    
+    def calculate_monthly_wage(self):
+        total_wage = 0
+        total_hrs = 0
+        total_days = 0
         
-        job=input('enter a type of job :').lower()     
-        if job=='part':
-          hrs=part_hr
-              
-        elif job=='full':
-          hrs=full_hr
-        else:
-          print("give job type")
-          continue       
-         
-        if total_hrs+hrs >= max_hr or total_days >= max_days:
-          break
-        total_hrs+=hrs
-        total_days+=1
-        total_wage+=wage_hr*hrs
+        while total_hrs < self.max_hr and total_days < self.max_days:
+            attendance = random.randint(0, 1)
+            if attendance == 0:
+                continue
             
-    print( "Total working days are" ,total_days) 
-    print("Total working hrs are'" ,total_hrs)           
-    return total_wage
+            job = input('Enter the type of job (full/part): ').lower()
+            
+            if job == 'part':
+                hrs = self.part_hr
+            elif job == 'full':
+                hrs = self.full_hr
+            else:
+                print("Invalid job type. Please enter 'full' or 'part'.")
+                continue
+            
+            if total_hrs + hrs > self.max_hr or total_days >= self.max_days:
+                break
+            
+            total_hrs += hrs
+            total_days += 1
+            total_wage += self.wage_hr * hrs
+        
+        print("Total working days:", total_days)
+        print("Total working hours:", total_hrs)
+        print("Total monthly wage:", total_wage)
 
 
- 
-print("Total monthly wage:" ,monthly_wage())
+EmployeeWage().calculate_monthly_wage()
 
-
-
+#we can skip using self we need to use cls and @classmethod decorator
 
 
 
