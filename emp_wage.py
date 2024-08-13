67ee8808ab24abdd4f8147a7698c9a23d5843458
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-14 00:46:56 +0530

    ADDED condition of days and hours

--- emp_wage.py
+++ emp_wage.py
@@ -1,22 +1,41 @@
-import random
+import random  
 print("Welcome to employee wage computation program")
-import random
+wage_hr=20
+full_hr=8
+part_hr=4
+max_hr=100
+max_days=20
 def monthly_wage():
     total_wage=0
-    for i in range(20):
+    total_hrs=0
+    total_days=0
+    while total_hrs<max_hr or total_days<max_days:
         attendance=random.randint(0,1)
         if attendance==0:
-            continue
-        if attendance==1:
-            job=input('enter a type of job :').lower()
-            if job=='part':
-                total_wage=total_wage+(20*6)
-            elif job=='full':
-                total_wage=total_wage+(20*8)
-            else:
-                print("give job type")
+          continue
+        
+        job=input('enter a type of job :').lower()     
+        if job=='part':
+          hrs=part_hr
+              
+        elif job=='full':
+          hrs=full_hr
+        else:
+          print("give job type")
+          continue       
+         
+        if total_hrs+hrs >= max_hr or total_days >= max_days:
+          break
+        total_hrs+=hrs
+        total_days+=1
+        total_wage+=wage_hr*hrs
+            
+    print( "Total working days are" ,total_days) 
+    print("Total working hrs are'" ,total_hrs)           
     return total_wage
 
+
+ 
 print("Total monthly wage:" ,monthly_wage())
 
 
