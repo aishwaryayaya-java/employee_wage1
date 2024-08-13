bc59c2ac40b55321df1fd89ead313f05d0d84ffa
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-13 15:59:19 +0530

    ADDED as switch case

--- emp_wage.py
+++ emp_wage.py
@@ -1,6 +1,26 @@
 import random
 print("Welcome to employee wage computation program")
+def check_attendance(attendance):
+    match (attendance):
+        case 0: 
+            print("Employee is absent")
+            dailywage=0  
+            
+        case 1: 
+            print("Employee is present")  
+            job=input("Enter full/part time :")
+            match (job):
+                case "full":
+                    dailywage=20*8
+                    
+                case "part":
+                    dailywage=20*4
+    return dailywage                      
+num=random.randint(0,1)
+print(check_attendance(num))                   
 
+
+"""
 def check_attendance(attendance):
     if attendance==0:
         print('employee is absent')
@@ -16,3 +36,4 @@ def check_attendance(attendance):
         print(dailywage)
 num=random.randint(0,1)
 check_attendance(num)
+"""
