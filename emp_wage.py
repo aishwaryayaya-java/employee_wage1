4ddaaf4967cdc2619d5bf1f786256fec478758cf
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-13 13:15:13 +0530

    ADDED functionality to check attedance

--- emp_wage.py
+++ emp_wage.py
@@ -1,1 +1,10 @@
+import random
 print("Welcome to employee wage computation program")
+
+def check_attendance(attendance):
+    if attendance==0:
+        print('employee is absent')
+    if attendance==1:
+        print('employee is present')
+num=random.randint(0,1)
+check_attendance(num)
