3dc54b4b96d76ea5eff6e871f4c404f9ecea2c02
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-13 13:54:24 +0530

    ADDED functionality to cal DailyWage

--- emp_wage.py
+++ emp_wage.py
@@ -4,7 +4,11 @@ print("Welcome to employee wage computation program")
 def check_attendance(attendance):
     if attendance==0:
         print('employee is absent')
+        dailywage=0
+        print(dailywage)
     if attendance==1:
         print('employee is present')
+        dailywage=20*8
+        print(dailywage)
 num=random.randint(0,1)
 check_attendance(num)
