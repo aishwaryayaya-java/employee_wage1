3a802a51299cdd2473bc5c917ac15a74272ae994
aishwaryayaya-java <aishwarya.sandhanshi_etc2020@pccoer.in>
2024-08-14 14:05:08 +0530

    ADDED randm no. to part/full

--- emp_wage.py
+++ emp_wage.py
@@ -1,28 +1,30 @@
 import random
 
 class EmployeeWage:
-    
+    #these are class variables 
     wage_hr = 20
     full_hr = 8
     part_hr = 4
     max_hr = 100
     max_days = 20
+    # if we create one more method called init diff from this method , they will be instance variables
+    def calculate_monthly_wage(self): #this is class method 
+        #self a reference to the instance of the class from which the method is being called
     
-    def calculate_monthly_wage(self):
         total_wage = 0
         total_hrs = 0
-        total_days = 0
+        total_days = 0 #Local Variables
         
         while total_hrs < self.max_hr and total_days < self.max_days:
-            attendance = random.randint(0, 1)
+            attendance = random.randint(0, 2)
             if attendance == 0:
                 continue
             
-            job = input('Enter the type of job (full/part): ').lower()
+            #job = input('Enter the type of job (full/part): ').lower()
             
-            if job == 'part':
+            if attendance == 1:
                 hrs = self.part_hr
-            elif job == 'full':
+            elif attendance == 2:
                 hrs = self.full_hr
             else:
                 print("Invalid job type. Please enter 'full' or 'part'.")
@@ -41,7 +43,7 @@ class EmployeeWage:
 
 
 EmployeeWage().calculate_monthly_wage()
-
+# we can also instantiate the class creating instance(obj) like employee1,employee2 for diff calculations
 #we can skip using self we need to use cls and @classmethod decorator
 
 
